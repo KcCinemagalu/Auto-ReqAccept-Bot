40d09f424ca3789d41bde17b674241d31b6320ab
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2024-01-09 12:08:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ import asyncio, datetime, time
 ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted KC ✅"
 START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use"
 
-API_ID = int(env.get('API_ID'))
-API_HASH = env.get('API_HASH')
-BOT_TOKEN = env.get('BOT_TOKEN')
-DB_URL = env.get('DB_URL')
-ADMINS = int(env.get('ADMINS'))
+API_ID = int(env.get('API_ID',"14356452"))
+API_HASH = env.get('API_HASH',"cac21249a0c6373a1b742afb8dbc9cb7")
+BOT_TOKEN = env.get('BOT_TOKEN',")
+DB_URL = env.get('DB_URL',"mongodb+srv://Nandesha:alabal18@cluster0.6qjsxnq.mongodb.net/?retryWrites=true&w=majority")
+ADMINS = int(env.get('ADMINS',"5720092781"))
 
 Dbclient = AsyncIOMotorClient(DB_URL)
 Cluster = Dbclient['Cluster0']
